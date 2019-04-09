7876b0ef87679e6023953fcab350861967a456fb
Volodymyr Mykytiuk <mvlabaratory@gmail.com>
2019-04-09 12:37:01 +0300

    Add project files.

---
+++ RD_University/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace RD_University
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
